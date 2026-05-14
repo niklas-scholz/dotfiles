e2f0d2682144e1b95b30149942c3be559081a498
Niklas Scholz <niklas.scholz@propelleraero.com.au>
2026-05-14 18:21:25 +1000

    some settings

--- src/dot_config/nvim/lua/plugins/hardtime.lua
+++ src/dot_config/nvim/lua/plugins/hardtime.lua
@@ -1,12 +1,39 @@
 return {
-  -- "m4xshen/hardtime.nvim",
-  -- lazy = false,
-  -- dependencies = { "MunifTanjim/nui.nvim" },
-  -- opts = {
-  --   disabled_keys = {
-  --     ["<Up>"] = false, -- Allow <Up> key
-  --     ["<Down>"] = false, -- Allow <Down> key
-  --   },
-  --   disable_mouse = false,
-  -- },
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {
+    -- Remove restrictions from movement keys
+    restricted_keys = {
+      ["h"] = {},
+      ["j"] = {},
+      ["k"] = {},
+      ["l"] = {},
+    },
+    -- Allow arrow keys by overriding the default disabled_keys
+    disabled_keys = {
+      ["<Up>"] = {},
+      ["<Down>"] = {},
+      ["<Left>"] = {},
+      ["<Right>"] = {},
+    },
+    disable_mouse = false,
+    hints = {
+      -- Matches 'A' followed by any number of characters (none in your case)
+      -- then escaping and pressing 'i'
+      ["A<Esc>i"] = {
+        message = function()
+          return "Use '$' to move to the end, or just stay in 'A' to type!"
+        end,
+        length = 3,
+      },
+      -- Also catching the '$i' habit while we're at it
+      ["%$i"] = {
+        message = function()
+          return "Use 'A' instead of '$i'"
+        end,
+        length = 2,
+      },
+    },
+  },
 }
